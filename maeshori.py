f0427f829b111fff98093d9c31483c0669ce0cf3
miki0810 <mickeymizuki@hotmail.co.jp>
2020-07-01 14:30:15 +0900

    first commit

---
+++ maeshori.py
@@ -0,0 +1,14 @@
+import sys
+import re
+ 
+path = './morgue.txt'
+bindata = open(path, "rb")
+lines = bindata.readlines()
+for line in lines:
+    text = line.decode('Shift_JIS')    # Shift_JISで読み込み
+    text = re.split(r'\r',text)[0]     # 改行削除
+    text = text.replace('｜','')       # ルビ前記号削除
+    text = re.sub(r'《.+?》','',text)    # ルビ削除
+    text = re.sub(r'［＃.+?］','',text)  # 入力者注削除
+    print(text)
+    file = open('data.txt','a',encoding='utf-8').write(text)  # UTF-8に変換
